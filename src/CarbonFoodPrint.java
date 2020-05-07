dc58e63a4ef998fe02655e0e251115f775b08617
mcheng <mcheng@mchengs-MacBook-Pro.local>
2020-05-07 08:41:09 -0400

    Adding gitignore and src directory

---
+++ src/CarbonFoodPrint.java
@@ -0,0 +1,8 @@
+public class CarbonFoodPrint
+{
+        public static void main( String[] args )
+        {
+                System.out.println( "A friendly carbon footprint calculator" );
+                System.exit( 0 );
+        }
+}
